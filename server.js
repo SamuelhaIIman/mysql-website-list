18a575e08ffde1b67592da77376fd5895118f9f6
Samuel Hållman <samuel.hollman@hotmail.com>
2025-02-19 14:41:37 +0200

    Working ejs connection!

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
 const express = require('express');
+const path = require('path');
+
 const app = express();
 
 app.set("view engine", "ejs");
 
+app.set('views', path.join(__dirname, 'views'));
+
 app.get('/', (req, res) => {
     res.render('home', { name: 'Akashdeep'});
 });
-const server = app.listen(4000, function() {
-    console.log('listening to port 4000')
+const PORT = 4000;
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
 });
